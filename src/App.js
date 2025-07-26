ecde3a7e522c3799cb5642599d251637a94842ce
Pawan Sharma <74698965+pabbico@users.noreply.github.com>
2025-07-26 17:10:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>IQLearner App</title>
+  </head>
+  <body>
+    <noscript>You need to enable JavaScript to run this app.</noscript>
+    <div id="root"></div>
+  </body>
+</html>root@ip-172-31-43-3:~/aws_codepipeline_multi_branch_config# cat src/App.js
 import React from 'react';
 
 function App() {
@@ -5,8 +18,8 @@ function App() {
 
   return (
     <div style={{ textAlign: 'center', marginTop: '100px' }}>
-      <h1>🚀 Welcome to IQLearner Portal</h1>
-      <h2>🌎 Current Environment: <span style={{ color: 'blue' }}>{env}</span></h2>
+      <h1>Welcome to Pawan's Demo for AWS CodePipeline</h1>
+      <h2>Current Environment: <span style={{ color: 'blue' }}>{env}</span></h2>
     </div>
   );
 }
