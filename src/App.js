1aed8a625558e7832e3937aa533a4ea51049bb2f
Admin <admin@Admins-MacBook-Air-2.local>
2025-07-26 11:09:46 +0530

    code first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  const env = process.env.REACT_APP_ENV || "unknown";
+
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>🚀 Welcome to IQLearner Portal</h1>
+      <h2>🌎 Current Environment: <span style={{ color: 'blue' }}>{env}</span></h2>
+    </div>
+  );
+}
+
+export default App;
