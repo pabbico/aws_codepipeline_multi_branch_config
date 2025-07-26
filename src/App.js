fe1e3d4711b4a4f9c1956eebff67ffd6705870fe
Pawan Sharma <74698965+pabbico@users.noreply.github.com>
2025-07-26 17:36:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>IQLearner App</title>
-  </head>
-  <body>
-    <noscript>You need to enable JavaScript to run this app.</noscript>
-    <div id="root"></div>
-  </body>
-</html>root@ip-172-31-43-3:~/aws_codepipeline_multi_branch_config# cat src/App.js
 import React from 'react';
 
 function App() {
